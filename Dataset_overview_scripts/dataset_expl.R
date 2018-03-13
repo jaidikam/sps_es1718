ad5781e01d385c407ab86e49e71ac6135230aed2
raiber <raiber.kurdi@gmail.com>
2018-03-13 21:31:14 +0100

    population plotting code

--- Dataset_overview_scripts/dataset_expl.R
+++ Dataset_overview_scripts/dataset_expl.R
@@ -1,2 +1,27 @@
 #this script shall load all final dataset files created in the preparation stage and include the code for displaying 
 # eplorative graphs and tables.
+
+
+# plotting the population for the all countries and the world
+
+setwd("C:/Users/Raiber/Desktop/SPL 2")
+if(!require("reshape2")) install.packages("reshape2");library("reshape2")
+if(!require("ggplot2")) install.packages("ggplot2");library("ggplot2")
+source("preparation_functions.R")
+#reading the data  
+world_population = read.csv("world_population.csv", stringsAsFactors = FALSE, sep = ",", header = TRUE)
+
+# stacks a set of columns into a single column of data to be able to process it 
+world_population = melt(world_population, id=c("Year"))
+
+# calculating the percentage of the change in the population
+for(i in unique(world_population$variable)){
+  # i is the name of the land 
+  #print(i)
+  world_population = calcPercFixBaseyear(world_population,"variable",i,"Year",1991,"value", "percentage")
+}
+
+ggplot(world_population) + geom_line(aes(x=Year, y=percentage, colour=variable)) +
+  scale_colour_manual(values=c("red","green","blue", "gray")) +
+  ylab(label="Growth Precentage") +
+  xlab("Year") 
