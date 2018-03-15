4fdaa8ea8fbc1640a74e137f2a0467af2a55026d
raiber <raiber.kurdi@gmail.com>
2018-03-15 18:29:10 +0100

    da

--- Dataset_overview_scripts/dataset_expl.R
+++ Dataset_overview_scripts/dataset_expl.R
@@ -7,6 +7,7 @@ source(".\\Helper_functions\\preparation_functions.R")
 
 if(!require("reshape2")) install.packages("reshape2");library("reshape2")
 if(!require("ggplot2")) install.packages("ggplot2");library("ggplot2")
+if(!require("data.table")) install.packages("data.table");library("data.table")
 
 #reading the data  
 world_population = read.csv(".\\Comman datasets\\world_population.csv", stringsAsFactors = FALSE, sep = ",", header = TRUE)
@@ -33,4 +34,38 @@ dev.off()
 
 
 # plotting the production amount of the selected products for the specified countries compared to the world
+
+world_production = read.csv(".\\Comman datasets\\world production.csv", stringsAsFactors = FALSE, sep = ",", header = TRUE)
+world_production$X = NULL
+colnames(world_production) = c("Area", "Item", "1991", "1992", "1993", "1994", "1995", "1996", "1997", "1998", "1999", "2000", "2001", "2002", "2003", "2004", "2005", "2006", "2007", "2008", "2009", "2010", "2011", "2012", "2013", "2014", "2015")
+
+world_production = as.data.frame(t(world_production))
+setDT(world_production, keep.rownames = TRUE)[]
+
+
+
+
+"Rice, paddy" 
+RD = world_production[world_production$Area == "Rwanda", ]
+RD = RD[RD$Item %in% c("Cassava", "Bananas", "Beans, dry", "Maize", "Sweet potatoes", "Potatoes", "Rice, paddy"),]
+unique(RD$Item)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
