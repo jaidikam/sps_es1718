f225ecb93ba7cc0aef4eee10e0b27bc8709ea8be
raiber <raiber.kurdi@gmail.com>
2018-03-16 21:04:15 +0100

    hj

---
+++ Helper_functions/exploration_functions.R
@@ -0,0 +1,29 @@
+
+#calculating the percentage of the change in a column's value on a fixed base year 
+calcPercFixBaseyear =  function(ds,namecol,cname,yearcol, baseyear,valuecol, perccol){
+  base = ds[ds[[yearcol]] == baseyear & ds[[namecol]] %in% cname, ][[valuecol]]
+  if(is.null(ds[[perccol]])){
+    ds[[perccol]] = 0 
+  }
+  #
+  for(i in baseyear: max(ds[[yearcol]])){
+    later = ds[ds[[yearcol]]==i & ds[[namecol]] %in% cname,][[valuecol]]
+    sub =  later - base
+    ds[ds[[yearcol]] == i & ds[[namecol]] %in% cname,][[perccol]] = (sub / base) * 100
+  }
+  return(ds)
+}
+
+
+# calculate the prcentage of changes in a colname value in a predefined year, where the base is for every change is the value from the previous year.
+calcPercPreBaseyear = function(ds, colname, yearcol){
+  for(i in ds[, yearcol]){
+    base = ds[ds[,yearcol] == i, colname]
+    later = ds[ds[,yearcol] == i+1, colname]
+    if(length(later) == 0L) break
+    sub =  later - base
+    ds[ds[,yearcol] == i+1, paste(colname, "perc", sep = "_")]= (sub / base) * 100
+  }
+  ds[ds[,yearcol] == min(ds[,yearcol]), paste(colname, "perc", sep = "_")]= 0
+  return(ds)
+}
